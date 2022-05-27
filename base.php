e92c08eae849b75439ed778f862f023b8f33a85c
Pedro-ls <pedrohenriquedasilva204@gmail.com>
2022-05-27 09:36:44 -0300

    arquivo base added

---
+++ base.php
@@ -0,0 +1,29 @@
+<?php
+
+
+$cod = $_POST["cod"];
+$nome = $_POST["nome"];
+$endereco = $_POST["endereco"];
+$cidade = $_POST["cidade"];
+$estado = $_POST["estado"];
+$telefone = $_POST["telefone"];
+$porc_comissao = $_POST["porc_comissao"];
+
+$codigo = $_POST["codigo"];
+$nome = $_POST["nome"];
+$endereco = $_POST["endereco"];
+$telefone = $_POST["telefone"];
+$limite_cred = $_POST["limite_cred"];
+$email = $_POST["email"];
+$CPF = $_POST["CPF"];
+$estado = $_POST["estado"];
+
+$id = $_POST["id"];
+$descricao = $_POST["descricao"];
+
+
+$cod  = $_POST["cod"];
+$nome  = $_POST["nome"];
+$preco  = $_POST["preco"];
+$qtd_estoque  = $_POST["qtd_estoque"];
+$unidade_medida  = $_POST["unidade"];
